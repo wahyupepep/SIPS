0f250ba66c76541e651be1da0ffa788928e6e5ba
wahyupepep <wahyufebri69@gmail.com>
2021-04-14 09:59:05 +0700

    planting seed

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User;
+        $user->name = "User";
+        $user->nip = "User";
+        $user->email = "user@mail.com";
+        $user->password = bcrypt('12345678'); 
+        $user->save();
+    }
+}
